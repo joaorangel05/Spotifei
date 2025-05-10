6b9ea9fd1c4a6c3bf1bdf0210d0428b0562043c5
JOAO VITOR MACIENTE RANGEL <unifjrangel@feinet.fei.edu.br>
2025-05-10 10:45:00 -0300

    pastas criadas

--- src/Spotifei/Main.java
+++ src/Spotifei/Main.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package javaapplication2;
+package Spotifei;
 
 /**
  *
  * @author unifjrangel
  */
-public class JavaApplication2 {
+public class Main {
 
     /**
      * @param args the command line arguments
