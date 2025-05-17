3e3efa580fcf0dd245e3025a6d8287f812aa1be4
Rangel <jvmaciente@gmail.com>
2025-05-17 11:13:52 -0300

    View Música

--- src/Model/ModelMusica.java
+++ src/Model/ModelMusica.java
@@ -1,13 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Model;
 
-/**
- *
- * @author unifjrangel
- */
+
 public class ModelMusica {
+    private String nome, artista, genero;
+
+    public ModelMusica() {
+    }
+
+    public ModelMusica(String nome, String artista, String genero) {
+        this.nome = nome;
+        this.artista = artista;
+        this.genero = genero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+    
+    
     
 }
+
